6d227e3a9ac24ea39ab79462d5ab6b9ce5652f01
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-10-14 17:46:53 -0600

    Fix mispelling of next

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(function(req, res, net) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, Post");
