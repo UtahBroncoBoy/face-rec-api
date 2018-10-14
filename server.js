944cb4c5569c02acd6b14fc086d45a8f456717d3
Jeff Fredericks <jeffwfredericks@gmail.com>
2018-10-14 16:47:34 -0600

    Test CORS errors.

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ const db = knex({
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.get('/', (req, res) => {res.send('It is working!');})
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
